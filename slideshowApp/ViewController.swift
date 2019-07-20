ca6760d4173e494c37dcaa541195e0b8fc888342
takuma.tanaka <takuma7823@icloud.com>
2019-07-20 16:34:11 +0900

    slideshowAppの作成

--- slideshowApp/ViewController.swift
+++ slideshowApp/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  slideshowApp
+//  SlideshowApp
 //
 //  Created by 田中拓磨 on 2019/07/20.
 //  Copyright © 2019 田中拓磨. All rights reserved.
@@ -9,12 +9,99 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var timer_sec: Float = 0
+    var timer: Timer!
+    
+    
+    
+    @IBOutlet weak var Image: UIImageView!
+    
+    
+    @objc func updateTimer(_ timer: Timer) {
+        if Image.image == imgA{
+            Image.image = imgB
+        }else if Image.image == imgB{
+            Image.image = imgC
+        }else if Image.image == imgC{
+            Image.image = imgA
+        }
+    }
+    
+    let imgA: UIImage! = UIImage(named:"IMG-1")
+    let imgB: UIImage! = UIImage(named:"IMG-2")
+    let imgC: UIImage! = UIImage(named:"IMG-3")
+    
+    
+    @IBAction func move(_ sender: Any) {
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+    }
+    
+    
+    @IBAction func nextButton(_ sender: Any) {
+        if Image.image == imgA{
+            Image.image = imgB
+        }else if Image.image == imgB{
+            Image.image = imgC
+        }else if Image.image == imgC{
+            Image.image = imgA
+        }
+        
+    }
+    
+    @IBOutlet weak var nextButton2: UIButton!
+    
+    @IBAction func backButton(_ sender: Any) {
+        if Image.image == imgA{
+            
+            Image.image = imgC
+        }else if Image.image == imgB{
+            Image.image = imgA
+        }else if Image.image == imgC{
+            Image.image = imgB
+        }
+    }
+    
+    @IBOutlet weak var backButton2: UIButton!
+    
+    @IBAction func playButton(_ sender: Any) {
+        if self.timer == nil {
+            self.timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(updateTimer(_:)), userInfo: nil, repeats: true)
+            nextButton2.isEnabled = false
+            backButton2.isEnabled = false
+        }else if self.timer != nil{
+            self.timer.invalidate()
+            self.timer = nil
+            nextButton2.isEnabled = true
+            backButton2.isEnabled = true
+            
+            
+        }
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Image.image = imgA
+        self.view.addSubview(Image)
     }
-
-
+    
+    override func prepare(for segue:UIStoryboardSegue, sender: Any? ){
+        let viewController2:ViewController2 = segue.destination as! ViewController2
+        if Image.image == imgA{
+            viewController2.x = 1
+        }else if Image.image == imgB{
+            viewController2.x = 2
+        }else if Image.image == imgC{
+            viewController2.x = 3
+        }
+    }
+    
+    
 }
 
---
+++ slideshowApp/ViewController2.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController2.swift
+//  SlideshowApp
+//
+//  Created by 田中拓磨 on 2019/07/12.
+//  Copyright © 2019 田中拓磨. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+    
+    @IBOutlet weak var bigImage: UIImageView!
+    
+    var x = 0
+    
+    let imgA: UIImage! = UIImage(named:"IMG-1")
+    let imgB: UIImage! = UIImage(named:"IMG-2")
+    let imgC: UIImage! = UIImage(named:"IMG-3")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        if x == 1{
+            bigImage.image = imgA
+        }else if x == 2{
+            bigImage.image = imgB
+        }else if x == 3{
+            bigImage.image = imgC
+        }
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
